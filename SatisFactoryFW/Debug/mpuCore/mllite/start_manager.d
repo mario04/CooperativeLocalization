08d9d4050ac7cc11b35dcff0ddc52b9141e385a8
Mario Ruiz <marioalexander04@gmail.com>
2016-05-24 17:05:12 +0200

    Initial commit

---
+++ SatisFactoryFW/Debug/mpuCore/mllite/start_manager.d
@@ -0,0 +1,13 @@
+mpuCore/mllite/start_manager.o: ../mpuCore/mllite/start_manager.c \
+ ../mpuCore/mllite/start_manager.h \
+ ../mpuCore/mllite/../../mpuCore/driver/include/mltypes.h \
+ ../mpuCore/mllite/../../mpuCore/driver/include/stdint_invensense.h \
+ ../mpuCore/mllite/../driver/include/log.h
+
+../mpuCore/mllite/start_manager.h:
+
+../mpuCore/mllite/../../mpuCore/driver/include/mltypes.h:
+
+../mpuCore/mllite/../../mpuCore/driver/include/stdint_invensense.h:
+
+../mpuCore/mllite/../driver/include/log.h:
---
+++ SatisFactoryFW/Debug/system/src/diag/Trace.d
@@ -0,0 +1,4 @@
+system/src/diag/Trace.o: ../system/src/diag/Trace.c \
+ D:\STM32Toolchain\projects\SatisFactory_NucleoProto\gitSatisFactoryFW\SatisFactoryFW\system\include/diag/Trace.h
+
+D:\STM32Toolchain\projects\SatisFactory_NucleoProto\gitSatisFactoryFW\SatisFactoryFW\system\include/diag/Trace.h:
